378c7bc8ea8165da27abc8d39d959dc2f6ea1b95
Lucas Lemos <lucas.lemos.dev@gmail.com>
2024-02-06 20:55:48 -0400

    app init config

--- ample-admin/dist/assets/js/app.init.js
+++ ample-admin/dist/assets/js/app.init.js
@@ -3,10 +3,10 @@ $(function () {
   $("#main-wrapper").AdminSettings({
     Layout: "vertical", // vertical | horizontal
     SidebarType: "full", // full / mini-sidebar
-    BoxedLayout: true, // true | false
+    BoxedLayout: false, // true | false
     Direction: "ltr", // ltr | rtl
     Theme: "light", // light | dark
-    ColorTheme: "Red_Theme", // Red_Theme | Aqua_Theme | Purple_Theme | Green_Theme | Cyan_Theme | Orange_Theme
+    ColorTheme: "Purple_Theme", // Red_Theme | Aqua_Theme | Purple_Theme | Green_Theme | Cyan_Theme | Orange_Theme
     cardBorder: false // true | false
   });
 });
